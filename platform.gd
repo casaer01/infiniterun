5b733c73b14e4c11112771009aca8b9ef326611d
Eros Casas <ecasas182015@gmail.com>
2024-10-03 21:43:52 -0500

    fixed platform size crash

--- platform.gd
+++ platform.gd
@@ -1,11 +1,11 @@
 extends TileMapLayer
 
 var speed: int = 100
-var platform_size:int = 2
+var platformSize:int = 1
 
 func set_platform():
-	for i in range(platform_size):
-		set_cell(Vector2(i,0), 0, Vector2(0,0), 0)
+	for i in range(platformSize):
+		set_cell(Vector2(i,0), 1, Vector2(0,0), 0)
 
-func _physics_process(delta: float):
+func _physics_process(delta: float) -> void:
 	position.x -= speed * delta
